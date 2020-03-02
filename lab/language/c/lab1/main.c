a54549bade98ee8fb541b143c3af7fc8fae43db3
JerryDing <deanroom@gmail.com>
2020-03-02 10:15:57 +0800

    add c code test

--- lab/language/c/lab1/main.c
+++ lab/language/c/lab1/main.c
@@ -1,1 +1,18 @@
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+#include <stdio.h>
+
+int main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0 ;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = (fahr-32) *(5.0/9.0);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
