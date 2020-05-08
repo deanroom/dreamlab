0b3d790c537a024180f280d50e9dd0bc1b4578d8
JerryDing <deanroom@gmail.com>
2020-05-08 23:56:29 +0800

    feat:add minigrep

--- lab/language/rust/hello_cargo/src/main.rs
+++ lab/language/rust/hello_cargo/src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = hello_cargo::Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = hello_cargo::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
