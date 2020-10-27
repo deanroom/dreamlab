c94a50c43a5d56760dd92b19c859cc7804f845cd
JerryDing <deanroom@gmail.com>
2020-10-28 02:55:40 +0800

    feat:Add gitnore of c

--- lab/language/c/lab1/main.c
+++ lab/language/c/lab1/main.c
@@ -1,18 +1,16 @@
-#include <stdio.h>
+#include <stdio.h> 
 
+int bitcount(unsigned x)
+{
+    int b;
+    for (b = 0; x !=0; x &= x-1)
+    {
+        b++;
+        printf("A loop of %d,and the number is %d \n",b,x);
+    }
+    return b;
+}
 int main()
 {
-    float fahr, celsius;
-    int lower, upper, step;
-
-    lower = 0 ;
-    upper = 300;
-    step = 20;
-
-    fahr = lower;
-    while (fahr <= upper) {
-        celsius = (fahr-32) *(5.0/9.0);
-        printf("%3.0f\t%6.1f\n", fahr, celsius);
-        fahr = fahr + step;
-    }
-}
+    printf("The number of 1 in x is %d.\n",bitcount(255));
+} 
