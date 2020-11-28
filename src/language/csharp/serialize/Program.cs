9d44576cbb3345ab53618845fcb18a97fc1dbbb1
JerryDing <deanroom@gmail.com>
2020-11-28 21:37:26 +0800

    feat: add a serialize example

---
+++ src/language/csharp/serialize/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace serialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            TypeNamingTest.Run();
+            
+        }
+    }
+}
---
+++ src/language/csharp/serialize/TypeNamingTest.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+
+namespace serialize
+{
+    public class TypeNamingTest
+    {
+        public static void Run()
+        {
+            var me = new Me()
+            {
+                Age = "37",
+                Salary = "000",
+                Persons = new List<Person>
+                {
+                    new Person() {FirstName = "Jerry", LastName = "Ding"},
+                    new Employee() {FirstName = "Jerry1", LastName = "Ding1",Department="It1"},
+                    new Me() {FirstName = "Jerry2", LastName = "Ding2",Department="It2",Age = "200",IntPtrValue=new IntPtr(10)},
+                }
+            };
+            
+            var json = JsonConvert.SerializeObject(me);
+            Console.WriteLine($"Normal Serialize:\n{json}");
+
+            var meAgain = JsonConvert.DeserializeObject<Person>(json);
+            var meAgainJson = JsonConvert.SerializeObject(meAgain);
+            Console.WriteLine($"Normal deserialize:\n{meAgainJson}");
+
+            var json1 = JsonConvert.SerializeObject(me, Formatting.Indented,
+                new JsonSerializerSettings
+                {
+                    TypeNameHandling = TypeNameHandling.All
+                });
+
+            Console.WriteLine($"TypeNameHandling serialize:\n{json1}");
+            JObject rss = JObject.Parse(json1);
+            var typeString = rss.First.Values().Aggregate((current, x) =>
+            {
+                return current.ToString() + x.ToString();
+            });
+            var type = Type.GetType(typeString.ToString());
+            var meAgain1 = JsonConvert.DeserializeObject(json1, type);
+            var meAgainJson1 = JsonConvert.SerializeObject(meAgain1);
+            Console.WriteLine($"Use the type get from json to deserialize and then normal serialize:\n{meAgainJson1}");
+
+            var meAgain2 = JsonConvert.DeserializeObject(json1, new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.All
+            });
+            var meAgainJson2 = JsonConvert.SerializeObject(meAgain2);
+            Console.WriteLine($"Use type naming handling deserialize and then normal serialize:\n{meAgainJson2}");
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+
+    public class Employee : Person
+    {
+        public string Department { get; set; }
+        public string JobTitle { get; set; }
+    }
+
+    public class Me : Employee
+    {
+        public IntPtr IntPtrValue { get; set; }
+        public string Age { get; set; }
+        public string Salary { get; set; }
+        public IList<Person> Persons { get; set; }
+    }
+}
