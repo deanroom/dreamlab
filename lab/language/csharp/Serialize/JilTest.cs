4ddfda0fa7f58d26e196a6eb279f046cc5c70920
JerryDing <deansroom@163.com>
2020-12-22 00:08:50 +0800

    feat:add test

---
+++ lab/language/csharp/Serialize/JilTest.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Jil;
+
+namespace serialize
+{
+    public class JilTest
+    {
+        public static void Run()
+        {
+            var me = new Me()
+            {
+                Age = "37",
+                Salary = "000",
+                Persons = new List<Person>
+                {
+                    new Person() {FirstName = "Jerry", LastName = "Ding"},
+                    new Employee() {FirstName = "Jerry1", LastName = "Ding1", Department = "It1"},
+                    new Me()
+                    {
+                        FirstName = "Jerry2", LastName = "Ding2", Department = "It2", Age = "200",
+                        IntPtrValue = new IntPtr(10)
+                    },
+                }
+            };
+            using (var output = new StringWriter())
+            {
+                JSON.Serialize(me,output);
+            }
+        }
+    }
+}
--- lab/language/csharp/Serialize/Program.cs
+++ lab/language/csharp/Serialize/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using Jil;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -12,14 +14,23 @@ namespace serialize
     {
         static void Main(string[] args)
         {
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            TypeNamingTest.Run();
-            Console.WriteLine("Cost Time {0}ms",sw.ElapsedMilliseconds);
-            sw.Restart();
-            BoisTest.Run();
-            Console.WriteLine("Cost Time {0}ms",sw.ElapsedMilliseconds);
-            
+            for (int i = 0; i < 10; i++)
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                TypeNamingTest.Run();
+                Console.WriteLine("Cost Time {0}ms",sw.ElapsedMilliseconds);
+                sw.Restart();
+                using (var output = new StringWriter())
+                {
+                    JSON.Serialize(new Me(){FirstName = i.ToString()},output);
+                }
+                Console.WriteLine("Cost Time {0}ms",sw.ElapsedMilliseconds);
+                
+                sw.Restart();
+                Utf8Json.Run();
+                Console.WriteLine("Cost Time {0}ms",sw.ElapsedMilliseconds);
+            }
         }
     }
 }
