198ad3443658d88a10bbb9536c0bc89af25dcfda
JerryDing <deanroom@gmail.com>
2021-01-09 23:12:42 +0800

    feat:finish a demo of protobuf dynamic register

--- lab/language/csharp/ProtobufNetTest/ProtobufNetTest.cs
+++ lab/language/csharp/ProtobufNetTest/ProtobufNetTest.cs
@@ -1,33 +1,131 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.ServiceModel.Channels;
 using Newtonsoft.Json;
 using ProtoBuf;
+using ProtoBuf.Meta;
 
 namespace ProtobufNetTest
 {
-    [ProtoContract]
+    [Message]
     public abstract class ProtoPerson
     {
-        [ProtoMember(1)] public string Name { get; set; }
+        public string Name { get; set; }
     }
 
-    [ProtoContract]
-    public class Student : ProtoPerson
+    [Message]
+    public class Student:ProtoPerson
     {
-        [ProtoMember(2)] public int Age { get; set; }
-        [ProtoMember(3)] public string Gender { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
+
+    [Message]
+    public class Student1 : ProtoPerson
+    {
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
+
+    [Message]
+    public class Student2 : Student
+    {
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
+
+    [Message]
+    public class Teacher : ProtoPerson
+    {
+        public int Age1 { get; set; }
+        public string Gender1 { get; set; }
+    }
+
+    [Message]
+    public class Teacher1 : Teacher
+    {
+        public int Age1 { get; set; }
+        public string Gender1 { get; set; }
+    }
+    public class MessageAttribute : Attribute
+    {
     }
 
     public class ProtobufNetTest
     {
+        public static Dictionary<Type, IList<Type>> Types { get; set; } = new Dictionary<Type, IList<Type>>();
+
         public static void Run()
         {
-            var student = new Student {Age = 10, Gender = "Male"};
+            Console.WriteLine(Assembly.GetCallingAssembly().FullName);
+            Console.WriteLine(Assembly.GetExecutingAssembly().FullName);
+            Console.WriteLine(Assembly.GetEntryAssembly()?.FullName);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            RegisterMessage();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+
+            var student = new Student {Age = 10, Gender = "Male",Name = "Test"};
             var newStudent = ProcessData(student);
             Console.WriteLine(JsonConvert.SerializeObject(newStudent));
         }
 
-        static T ProcessData<T>(T data) where T : ProtoPerson
+        private static void RegisterMessage()
+        {
+            var types = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(x => x.GetCustomAttribute<MessageAttribute>() != null);
+            foreach (var type in types)
+            {
+                RegisterType(type);
+            }
+            foreach (var derivedType in Types)
+            {
+                var number = 1;
+                var model = RuntimeTypeModel.Default.Add(derivedType.Key, true);
+
+                foreach (var type in derivedType.Value)
+                {
+                    model.AddSubType(number++, type);
+                }
+                var members = derivedType.Key.GetMembers();
+                foreach (var memberInfo in members)
+                {
+                    if (memberInfo.MemberType == MemberTypes.Property)
+                    {
+                        model.AddField(number++, memberInfo.Name);
+                    }
+                }
+                Console.WriteLine(
+                    $"key:{derivedType.Key},Values:{string.Join(",", derivedType.Value?.Select(x => x.Name).ToArray())}");
+            }
+        }
+
+        private static void RegisterType(Type type)
+        {
+            if (type.Name != nameof(ProtoPerson))
+            {
+                var baseType = type.BaseType;
+                if (baseType == null)
+                    return;
+                RegisterType(baseType);
+            }
+
+            if (!Types.ContainsKey(type))
+            {
+                Types.Add(type, new List<Type>());
+            }
+
+            if (type.BaseType != null && Types.ContainsKey(type.BaseType))
+            {
+                Types[type.BaseType].Add(type);
+            }
+        }
+
+        static T ProcessData<T>(T data)
         {
             using (var ms = new MemoryStream())
             {
@@ -35,8 +133,7 @@ namespace ProtobufNetTest
                 var buffer = ms.ToArray();
                 using (var ms2 = new MemoryStream(buffer))
                 {
-                    var result= Serializer.Deserialize<T>(ms2);
-                    Console.WriteLine(result.Name);
+                    var result = Serializer.Deserialize<T>(ms2);
                     return result;
                 }
             }
