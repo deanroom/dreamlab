51f7566dc9992ffe950244057ecfd364d42bbfff
JerryDing <deanroom@gmail.com>
2021-01-15 20:02:54 +0800

    FEAT:ADD TEST

--- lab/language/csharp/Threading/Program.cs
+++ lab/language/csharp/Threading/Program.cs
@@ -7,7 +7,7 @@ namespace ThreadingTest
     {
         static void Main(string[] args)
         {
-            ThreadingContextTest.Run();
+            EventThreadingTest.Run();
         }
     }
 }
