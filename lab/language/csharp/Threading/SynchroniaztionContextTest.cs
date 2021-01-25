8bd9a223c5c80a562514f9727e758b34092269c2
JerryDing <deanroom@gmail.com>
2021-01-25 08:56:51 +0800

    feat:update content

--- lab/language/csharp/Threading/SynchroniaztionContextTest.cs
+++ lab/language/csharp/Threading/SynchroniaztionContextTest.cs
@@ -8,57 +8,46 @@ namespace ThreadingTest
     {
         public static void Run()
         {
-            var context = new SynchronizationContext();
-            Task.Run(() =>
-            {
-                Console.WriteLine($"[Task]This is a thread scope of {Thread.CurrentThread.ManagedThreadId}");
-                context.Post(
-                    (state) =>
-                    {
-                        Console.WriteLine(
-                            $"[Task-Context-Post]This is a post operation from thread {Thread.CurrentThread.ManagedThreadId}");
-                    },default);
-                context.Post(
-                    (state) =>
-                    {
-                        Console.WriteLine(
-                            $"[Task-Context-Post]This is a post operation from thread {Thread.CurrentThread.ManagedThreadId}");
-                    },default);
-                context.Send(
-                    (state) =>
-                    {
-                        Console.WriteLine(
-                            $"[Task-Context-Send]This is a post operation from thread {Thread.CurrentThread.ManagedThreadId}");
-                    },default);
-                context.Send(
-                    (state) =>
-                    {
-                        Console.WriteLine(
-                            $"[Task-Context-Send]This is a post operation from thread {Thread.CurrentThread.ManagedThreadId}");
-                    },default);
-            });
-            context.Post(
-                (state) =>
-                {
-                    Console.WriteLine($"[Context-Post]This is a post operation from thread {Thread.CurrentThread.ManagedThreadId}");
-                }, default);
-            context.Post(
-                (state) =>
-                {
-                    Console.WriteLine($"[Context-Post]This is a post operation from thread {Thread.CurrentThread.ManagedThreadId}");
-                }, default);
-            context.Send(
-                (state) =>
-                {
-                    Console.WriteLine($"[Context-Send]This is a post operation from thread {Thread.CurrentThread.ManagedThreadId}");
-                }, default);
-            context.Send(
-                (state) =>
-                {
-                    Console.WriteLine($"[Context-Send]This is a post operation from thread {Thread.CurrentThread.ManagedThreadId}");
-                }, default);
+            Console.WriteLine($"[Main]This is a thread scope of {Thread.CurrentThread.ManagedThreadId}");
 
-            Console.WriteLine($"This is normal operation from thread {Thread.CurrentThread.ManagedThreadId}");
+            Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine($"[Task-Factory]This is a thread scope of {Thread.CurrentThread.ManagedThreadId}");
+
+                SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
+                SynchronizationContext.Current.Post((state)=>{
+                    Console.WriteLine($"[Task-Post]This is a thread scope of {Thread.CurrentThread.ManagedThreadId}");
+                },1);
+
+            },TaskCreationOptions.LongRunning);
+            // Task.Run(() =>
+            // {
+            //     Console.WriteLine($"[Task]This is a thread scope of {Thread.CurrentThread.ManagedThreadId}");
+            //     context.Post(
+            //         (state) =>
+            //         {
+            //             Console.WriteLine(
+            //                 $"[Task-Context-Post]This is a post operation in thread {Thread.CurrentThread.ManagedThreadId}");
+            //         },default);
+      
+            //     context.Send(
+            //         (state) =>
+            //         {
+            //             Console.WriteLine(
+            //                 $"[Task-Context-Send]This is a send operation in thread {Thread.CurrentThread.ManagedThreadId}");
+            //         },default);
+            // });
+            // context.Post(
+            //     (state) =>
+            //     {
+            //         Console.WriteLine($"[Context-Post]This is a post operation in thread {Thread.CurrentThread.ManagedThreadId}");
+            //     }, default);
+            // context.Send(
+            //     (state) =>
+            //     {
+            //         Console.WriteLine($"[Context-Send]This is a send operation in thread {Thread.CurrentThread.ManagedThreadId}");
+            //     }, default);
+
         }
     }
 }
