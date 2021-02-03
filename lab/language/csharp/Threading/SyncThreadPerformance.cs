336e0c2943d1223238ca24bd97d9c9a5135bc971
JerryDing <deanroom@gmail.com>
2021-02-03 13:36:31 +0800

    feat:增加测试

--- lab/language/csharp/Threading/SyncThreadPerformance.cs
+++ lab/language/csharp/Threading/SyncThreadPerformance.cs
@@ -1,55 +1,76 @@
 using System;
 using System.Collections.Concurrent;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using NetMQ;
 
 namespace ThreadingTest
 {
+    public class Message
+    {
+        public int Id { get; set; }
+        public long Start { get; set; }
+        public long End { get; set; }
+    }
+
     public class SyncThreadPerformance
     {
         private static NetMQPoller poller = new();
-        private static ConcurrentQueue<int> writeQueue = new();
-        private static ConcurrentQueue<int> readQueue = new();
+        private static ConcurrentQueue<Message> writeQueue = new();
+        private static ConcurrentQueue<Message> readQueue = new();
 
         public static void Run()
         {
-            var number = 1000;
+            var number = 100;
             poller.RunAsync();
             var task1 = Task.Factory.StartNew(() =>
                 {
                     for (int i = 0; i < number; i++)
                     {
-                        poller.Run(() => { writeQueue.Enqueue(i); });
+                        poller.Run(() =>
+                        {
+                            writeQueue.Enqueue(new Message() {Id = i, Start = Stopwatch.GetTimestamp()});
+                        });
                     }
                 },
                 TaskCreationOptions.LongRunning);
-            
-            Stopwatch sw = new Stopwatch();
+
             var task2 = Task.Factory.StartNew(() =>
             {
                 var getCount = 0;
-                sw.Start();
                 while (true)
                 {
                     if (writeQueue.TryDequeue(out var value))
                     {
+                        value.End = Stopwatch.GetTimestamp();
+                        readQueue.Enqueue(value);
                         getCount++;
                     }
 
                     if (getCount == number)
                         break;
                 }
-
-                Console.WriteLine($"总共耗时:{sw.ElapsedMilliseconds}ms");
             }, TaskCreationOptions.LongRunning);
             Task.WaitAll(task2);
+            foreach (var message in readQueue)
+            {
+                Console.WriteLine($"总共耗时:{(message.End - message.Start).GetMicroSeconds()}µs");
+            }
         }
 
         private static void ShowThreadId(string scope)
         {
             Console.WriteLine($"{scope}:{Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+
+    public static class Extensions
+    {
+        public static long GetMicroSeconds(this long timeStamp)
+        {
+            return timeStamp / (Stopwatch.Frequency / 1_000_000);
         }
     }
 }
